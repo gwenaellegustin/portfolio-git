469304990a5caf138ea711692eb37e7c80078c6e
Gwenaëlle Gustin <gwenaelle.gustin@hevs.ch>
2026-01-30 16:28:36 +0100

    responsive and 10 hor lines

--- src/app/pages/timeline/timeline.ts
+++ src/app/pages/timeline/timeline.ts
@@ -1,11 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  Inject,
-  PLATFORM_ID,
-  ViewChild,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-timeline',
@@ -18,22 +11,19 @@ export class Timeline {
   @ViewChild('canvas')
   canvasRef!: ElementRef;
   private ctx!: CanvasRenderingContext2D;
-  isBrowser = false;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor() {}
 
   ngAfterViewInit() {
+    window.addEventListener('resize', () => this.resizeCanvas(), false);
     this.resizeCanvas();
-    window.addEventListener('resize', this.draw, false);
   }
 
   resizeCanvas() {
-    console.log('resize');
-    const canvasEl = this.canvasRef.nativeElement as HTMLCanvasElement;
-    canvasEl.style.width = '100vw';
-    canvasEl.style.height = '100vh';
-    canvasEl.width = canvasEl.offsetWidth;
-    canvasEl.height = canvasEl.offsetHeight;
+    this.canvasRef.nativeElement.style.width = '100%';
+    this.canvasRef.nativeElement.style.height = '1000px';
+    this.canvasRef.nativeElement.width = this.canvasRef.nativeElement.offsetWidth;
+    this.canvasRef.nativeElement.height = this.canvasRef.nativeElement.offsetHeight;
 
     this.draw();
   }
@@ -41,33 +31,44 @@ export class Timeline {
   public draw() {
     this.ctx = this.canvasRef.nativeElement.getContext('2d');
     this.ctx.beginPath();
-
     const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
     const width = canvas.width;
     const height = canvas.height;
     const centerX = width / 2;
+    const fontSize = 20;
+    this.ctx.font = fontSize.toString() + 'px Numans';
+    const radius = 10;
+    const marginTop = 20;
 
+    this.ctx.save();
+
+    // Main vertical line
     this.ctx.strokeStyle = 'black';
     this.ctx.lineWidth = 2;
     this.ctx.lineCap = 'butt';
-
-    this.ctx.moveTo(centerX, 0);
+    this.ctx.moveTo(centerX, 10);
     this.ctx.lineTo(centerX, height);
     this.ctx.stroke();
+    this.ctx.restore();
 
-    this.ctx.save();
+    // Dot today
+    this.ctx.beginPath();
+    this.ctx.arc(centerX, marginTop, radius, 0, 2 * Math.PI);
+    this.ctx.fillStyle = 'black';
+    this.ctx.fill();
+    this.ctx.stroke();
+    this.ctx.fillText('09.06.1996: 30 years old', centerX + radius * 2, marginTop + fontSize / 4);
+    this.ctx.restore();
 
     // Dashed lines
     this.ctx.setLineDash([2, 2]); // dotted pattern: 4px dash, 8px gap
     this.ctx.lineWidth = 1;
-
-    console.log(height);
-    const start = width * 0.1;
-    console.log(start);
-
-    for (let y = start; y <= height; y += height / 3) {
-      this.ctx.font = '48px serif';
-      this.ctx.fillText('2026', 10, 50);
+    let year = 2027;
+    for (let y = height / 10; y <= height; y += height / 10) {
+      // text
+      year = year - 1;
+      this.ctx.fillText(year.toString(), 10, y - 5);
+      // line
       this.ctx.beginPath();
       this.ctx.moveTo(0, y); // 0.5 for crisper 1px lines on canvas
       this.ctx.lineTo(width, y);
