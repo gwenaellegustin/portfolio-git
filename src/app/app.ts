4161401e4fed33ce16f93a7ac47d0abdba394e08
Gwenaëlle Gustin <gustin.gwenaelle96@gmail.com>
2026-02-13 20:05:57 +0100

    2 timelines and hover effect

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footer } from './components/footer/footer';
-import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Navbar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/pages/timeline/timeline.ts
+++ src/app/pages/timeline/timeline.ts
@@ -10,77 +10,17 @@ import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angu
 export class Timeline {
   @ViewChild('canvas')
   canvasRef!: ElementRef;
-  private ctx!: CanvasRenderingContext2D;
+  description = 'Welcome on my website. Discover my project trough the timelines';
 
   constructor() {}
 
-  ngAfterViewInit() {
-    window.addEventListener('resize', () => this.resizeCanvas(), false);
-    // this.resizeCanvas();
-  }
-
-  resizeCanvas() {
-    this.canvasRef.nativeElement.style.width = '100%';
-    this.canvasRef.nativeElement.style.height = '1000px';
-    this.canvasRef.nativeElement.width = this.canvasRef.nativeElement.offsetWidth;
-    this.canvasRef.nativeElement.height = this.canvasRef.nativeElement.offsetHeight;
-
-    this.draw();
-  }
-
-  public draw() {
-    this.ctx = this.canvasRef.nativeElement.getContext('2d');
-    this.ctx.beginPath();
-    const canvas = this.canvasRef.nativeElement as HTMLCanvasElement;
-    const width = canvas.width;
-    const height = canvas.height;
-    const centerX = width / 2;
-    const fontSize = 20;
-    this.ctx.font = fontSize.toString() + 'px tosh-a';
-    const radius = 10;
-    const marginTop = 20;
-
-    this.ctx.save();
-
-    // Main vertical line
-    this.ctx.strokeStyle = 'black';
-    this.ctx.lineWidth = 2;
-    this.ctx.lineCap = 'butt';
-    this.ctx.moveTo(centerX, 10);
-    this.ctx.lineTo(centerX, height);
-    this.ctx.stroke();
-    this.ctx.restore();
-
-    // Dot today
-    this.ctx.beginPath();
-    this.ctx.arc(centerX, marginTop, radius, 0, 2 * Math.PI);
-    this.ctx.fillStyle = 'black';
-    this.ctx.fill();
-    this.ctx.stroke();
-    this.ctx.fillText('09.06.1996: 30 years old', centerX + radius * 2, marginTop + fontSize / 4);
-    this.ctx.restore();
-
-    // Dashed lines
-    this.ctx.setLineDash([2, 2]); // dotted pattern: 4px dash, 8px gap
-    this.ctx.lineWidth = 1;
-    let year = 2027;
-    for (let y = height / 10; y <= height; y += height / 10) {
-      // text
-      year = year - 1;
-      this.ctx.fillText(year.toString(), 10, y - 5);
-      // line
-      this.ctx.beginPath();
-      this.ctx.moveTo(0, y); // 0.5 for crisper 1px lines on canvas
-      this.ctx.lineTo(width, y);
-      this.ctx.stroke();
+  onHover(hovered: string) {
+    if (hovered == 'black') {
+      this.description = 'Main timeline';
+    } else if (hovered == 'red') {
+      this.description = 'Red Red Red Red Red Red timeline';
+    } else {
+      this.description = 'Welcome on my website. Discover my project trough the timelines';
     }
-
-    this.ctx.setLineDash([]);
-    this.ctx.restore();
-
-    // ...
-
-    // Draw
-    this.ctx.fill();
   }
 }
