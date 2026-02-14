e2ee639c86f42325378a74670a863f95db85d9e7
Gwenaëlle Gustin <gustin.gwenaelle96@gmail.com>
2026-02-14 01:00:17 +0100

    5 timelines and color change on hover

--- src/app/pages/timeline/timeline.ts
+++ src/app/pages/timeline/timeline.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-timeline',
@@ -8,19 +8,42 @@ import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angu
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Timeline {
-  @ViewChild('canvas')
-  canvasRef!: ElementRef;
-  description = 'Welcome on my website. Discover my project trough the timelines';
+  defaultDescription = 'Welcome on my website. Discover my project trough the timelines';
+  description = this.defaultDescription;
+  descriptionDXD = 'DXD description';
+  descriptionHES = 'HES description';
+  descriptionHESproject = 'Project HES';
+  descriptionPM = 'PM description';
+  descriptionUp4it = 'Up4it description';
 
   constructor() {}
 
   onHover(hovered: string) {
-    if (hovered == 'black') {
+    let descriptionElement = document.getElementById('description');
+    if (!descriptionElement) {
+      return;
+    }
+
+    if (hovered == 'main') {
       this.description = 'Main timeline';
-    } else if (hovered == 'red') {
-      this.description = 'Red Red Red Red Red Red timeline';
+    } else if (hovered == 'dxd') {
+      this.description = this.descriptionDXD;
+      descriptionElement.setAttribute('style', 'color:#354be2');
+    } else if (hovered == 'HES') {
+      this.description = this.descriptionHES;
+      descriptionElement.setAttribute('style', 'color:#4fa8e6');
+    } else if (hovered == 'HES-project') {
+      this.description = this.descriptionHESproject;
+      descriptionElement.setAttribute('style', 'color:#4fa8e6');
+    } else if (hovered == 'pm') {
+      this.description = this.descriptionPM;
+      descriptionElement.setAttribute('style', 'color:#ce5e5d');
+    } else if (hovered == 'up4it') {
+      this.description = this.descriptionUp4it;
+      descriptionElement.setAttribute('style', 'color:#41d8ae');
     } else {
-      this.description = 'Welcome on my website. Discover my project trough the timelines';
+      this.description = this.defaultDescription;
+      descriptionElement.setAttribute('style', 'color:black');
     }
   }
 }
