5cd4a368207e9bd99a38cc270aff56430b7f6e21
Gwenaëlle Gustin <gustin.gwenaelle96@gmail.com>
2026-02-15 14:31:10 +0100

    project not found

--- src/app/pages/project/project.ts
+++ src/app/pages/project/project.ts
@@ -11,8 +11,8 @@ import { ActivatedRoute } from '@angular/router';
 export class Project {
   project: ProjectInterface = {
     title: 'Project now found',
-    description: 'TODO',
-    imageUrl: '',
+    description: 'Please check the url.',
+    imageUrl: './logo/GG_Racoon_Body.png',
   };
 
   constructor(private route: ActivatedRoute) {
@@ -27,10 +27,14 @@ export class Project {
   }
 }
 
-export interface ProjectInterface {
-  title: string;
-  description: string;
-  imageUrl: string;
+export class ProjectInterface {
+  title: string = '';
+  date?: string;
+  subtitle?: string;
+  gitUrl?: string;
+  siteUrl?: string;
+  description: string = '';
+  imageUrl: string = '';
 }
 
 export const projects = new Map<string, ProjectInterface>([
@@ -38,10 +42,23 @@ export const projects = new Map<string, ProjectInterface>([
     'scg',
     {
       title: 'Swiss Cyber grid',
+      date: '12.2024',
+      subtitle: 'Angular 18, mobile-friendly',
+      gitUrl: 'https://github.com/GeeeHesso/AramisFrontend',
+      siteUrl: 'https://swisscybergrid.iigweb.hevs.ch/',
       description:
         'The challenge of this project was to represent 2 maps highlighting the impact of a computer attack on Swiss power grid data. The project was carried out as part of my work at HES-SO, in the lab of Professor David Wannier. It was the first project I managed. Backend has been developed by Marc Gillioz.',
       imageUrl: './hes/swisscybergrid/mini.png',
     },
   ],
-  ['vst', { title: 'Visualization tool', description: 'TODO', imageUrl: '' }],
+  [
+    'vst',
+    {
+      title: 'Visualization tool',
+      date: '07.2025',
+      subtitle: 'Angular 20, mobile-friendly',
+      description: 'TODO',
+      imageUrl: './hes/visualization_tool/mockup.jpg',
+    },
+  ],
 ]);
