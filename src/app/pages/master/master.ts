8eecaef4b7f071db1c886f299a390de5adeca670
Gwenaëlle Gustin <gustin.gwenaelle96@gmail.com>
2026-02-15 18:07:40 +0100

    small fix

--- src/app/pages/master/master.ts
+++ src/app/pages/master/master.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Project } from '../project/project';
 
 @Component({
   selector: 'app-master',
-  imports: [Project],
+  imports: [],
   templateUrl: './master.html',
   styleUrls: ['./master.scss', '../project/project.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
