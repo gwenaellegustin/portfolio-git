57993880fa1d10903cd3aa4d6952245a276fd8ae
Gwenaëlle Gustin <gwenaelle.gustin@hevs.ch>
2026-06-02 16:47:50 +0200

    start building grid + data

--- src/app/pages/timeline/timeline.ts
+++ src/app/pages/timeline/timeline.ts
@@ -13,3 +13,31 @@ export class Timeline {
   readonly projectsService = inject(ProjectsService);
   constructor() {}
 }
+
+export class TimeLineInterface {
+  title: string = '';
+  yearStart: number = 0;
+  monthStart: number = 0;
+  yearEnd: number = 0;
+  monthEnd: number = 0;
+}
+export const timeline = new Map<string, TimeLineInterface>([
+  [
+    'MAIN',
+    {
+      title: 'main',
+      yearStart: 1996,
+      monthStart: 6,
+      yearEnd: 2026,
+      monthEnd: 6,
+    },
+  ],
+]);
+
+function monthDiff(y1: number, m1: number, y2: number, m2: number) {
+  var months;
+  months = (y2 - y1) * 12;
+  months -= m1;
+  months += m2;
+  return months <= 0 ? 0 : months;
+}
