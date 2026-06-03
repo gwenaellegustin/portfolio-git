4db43c5c8f5e899dabdda674799538e2263570dd
Gwenaëlle Gustin <gwenaelle.gustin@hevs.ch>
2026-06-03 12:27:31 +0200

    click open  project full width (but bottom of the page)

--- src/app/pages/timeline/mini-project/mini-project.ts
+++ src/app/pages/timeline/mini-project/mini-project.ts
@@ -23,6 +23,24 @@ export class MiniProject {
 
   ngOnInit() {}
 
+  openProject(project: string) {
+    const emptyLines = document.getElementsByClassName('project-line');
+    console.log(emptyLines);
+    for (let element of emptyLines) {
+      element.classList.replace('line', 'hidden');
+    }
+    const miniProjects = document.getElementsByClassName('project-container');
+    console.log(miniProjects);
+    for (let element of miniProjects) {
+      if (element.id == project) {
+        element.classList.replace('line', 'expand');
+      } else {
+        element.classList.replace('line', 'hidden');
+        console.log('removing', element);
+      }
+    }
+  }
+
   changeSizeTimeline() {
     // @TODO: find all div which id include project.month  +"."+ project.year and change size by height of current div project-container
   }
